058e83669a55c2684c81ba0cc81b67914add6391
Shu Yu <shu.yu@omnilogic.net>
2016-04-21 21:21:35 -0600

    Initial Commit

---
+++ GameAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameAnimation
+//
+//  Created by Chengshu Yu on 2016-04-21.
+//  Copyright © 2016 Chengshu Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameAnimation
+//
+//  Created by Chengshu Yu on 2016-04-21.
+//  Copyright © 2016 Chengshu Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
